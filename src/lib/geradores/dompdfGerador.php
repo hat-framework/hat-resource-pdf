c234dfc26656947a4eb94b4d6a1b08b37e233234
tigredonorte <tigredonorte3@gmail.com>
2015-09-30 19:56:44 -0300

    dompdfChances

--- src/lib/geradores/dompdfGerador.php
+++ src/lib/geradores/dompdfGerador.php
@@ -4,12 +4,18 @@ use classes\Classes\Object;
 class dompdfGerador extends classes\Classes\Object {
     
     public function __construct(){
-    
-    	require_once( dirname(__FILE__). "/files/dompdf_config.inc.php");
+        define('DOMPDF_ENABLE_AUTOLOAD'      , false);
+        define('DOMPDF_UNICODE_ENABLED'      , true);
+        define('DOMPDF_ENABLE_PHP'           , true);
+        define('DOMPDF_ENABLE_REMOTE'        , true);
+        define('DOMPDF_ENABLE_CSS_FLOAT'     , true);
+        define('DOMPDF_ENABLE_JAVASCRIPT'    , true);
+        define('DOMPDF_ENABLE_HTML5PARSER'   , true);
+        define('DOMPDF_ENABLE_FONTSUBSETTING', true);
         set_time_limit(0);
         ini_set('memory_limit','512M');
+        require_once BASE_DIR.'/vendor/dompdf/dompdf/dompdf_config.inc.php';
     	$this->dompdf = new DOMPDF();
-    	
     }
     
     static private $instance;
@@ -38,19 +44,22 @@ class dompdfGerador extends classes\Classes\Object {
     }
     
     public function pdfSaveToServer($html, $filename, $dir="", $orientation = 'portrait'){
-        
         $this->dompdf->load_html($html);
         $this->dompdf->set_paper('letter', $orientation);
         $this->dompdf->render();
         
     	//configs do seu DOMPDF….
-		$pdfoutput = $this->dompdf->output();
-		$filename = DIR_DEFAULT_UPLOAD."/dompdf/$dir/$filename.pdf";
-		$fp = fopen($filename, "a");
-		fwrite($fp, $pdfoutput);
-		fclose($fp);
+        $pdfoutput = $this->dompdf->output();
+        $file      = "$dir/$filename";
+        $this->LoadResource('files/dir', 'dobj')->create($dir, "");
+        
+        $f         = str_replace(".pdf.pdf", '.pdf', "$file.pdf");
+        $fp        = fopen($f, "a");
+        if(false === $fp){
+            die("erro");
+        }
+        fwrite($fp, $pdfoutput);
+        fclose($fp);
     }
     
 }
-
-?>
